578f6664089218cfb10782e465bb66894d01e069
archana.nagaraj <archana.nagaraj@gmail.com>
2012-01-28 09:29:59 +0000

    added buttons

--- letterBdetails.java
+++ letterBdetails.java
@@ -1,6 +1,5 @@
 package com.example.learnABCD;
 
-import android.animation.IntEvaluator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,27 +11,18 @@ import android.widget.ImageButton;
 public class letterBdetails extends Activity implements OnClickListener
 {
 	
-	ImageButton letterA;
+	
 	ImageButton letterC;
 	Button mainmenub;
 @Override
-protected void onCreate(Bundle savedInstanceState) {
+protected void onCreate(Bundle savedInstanceState) 
+{
 	// TODO Auto-generated method stub
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.b);
-	letterA=(ImageButton)findViewById(R.id.imageButtonA);
-	letterC=(ImageButton)findViewById(R.id.imageButtonC);
-	mainmenub=(Button)findViewById(R.id.menub);
-	letterA.setOnClickListener(new OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-		Intent Intentb=new Intent(letterBdetails.this,letterAdetails.class);
-		startActivity(Intentb);
-		}
-	});
 	
+	letterC=(ImageButton)findViewById(R.id.imageButton1);
+	mainmenub=(Button)findViewById(R.id.mainmenuB);
 	mainmenub.setOnClickListener(new OnClickListener() {
 		
 		@Override
@@ -44,17 +34,17 @@ protected void onCreate(Bundle savedInstanceState) {
 		}
 	});
 	
-/*	letterC.setOnClickListener(new OnClickListener() {
+	
+letterC.setOnClickListener(new OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 			Intent Intentc=new Intent(letterBdetails.this,letterCdetails.class);
-			startActivity(Intentc);
-			
+			startActivity(Intentc);			
 		}
 	});
-*/
+
 	
 }
 
@@ -66,3 +56,4 @@ protected void onCreate(Bundle savedInstanceState) {
 	}
 
 }
+
