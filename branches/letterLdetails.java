327b3a37b2d6efe3550c1d0d1d1401b7abea238d
archana.nagaraj@gmail.com <archana.nagaraj@gmail.com@271b1277-7c4e-8ac8-e3bd-6bdfaf7e2919>
2012-02-07 11:01:33 +0000

    L

---
+++ branches/letterLdetails.java
@@ -0,0 +1,58 @@
+package com.example.learnABCD;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class letterLdetails extends Activity implements OnClickListener
+{
+	
+	
+	ImageButton letterM;
+	Button mainmenuL;
+@Override
+protected void onCreate(Bundle savedInstanceState) 
+{
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.l);
+	
+	letterM=(ImageButton)findViewById(R.id.imageButton1);
+	mainmenuL=(Button)findViewById(R.id.mainmenuL);
+	mainmenuL.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent Intentmainmenul=new Intent(letterLdetails.this,LearnABCDActivity.class);
+			startActivity(Intentmainmenul);
+			
+		}
+	});
+	
+	
+letterM.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent Intentm=new Intent(letterLdetails.this,letterMdetails.class);
+			startActivity(Intentm);			
+		}
+	});
+
+	
+}
+
+
+@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
